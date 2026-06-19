bf5fb33b7ffb35da0f819c9ffcdf0b764ac46c7c
bob <bobli8203@gmail.com>
2026-06-19 15:53:15 -0400

    skeleton code for L2 cache and L1 cache (direct-mapped), still need to implement more ensuring working state and also need unittest upon working state achieved

--- rtl/src/memory.v
+++ rtl/src/memory.v
@@ -9,49 +9,71 @@
 // 32 - 10 = 22 bit tag
 
 module L1_cache(
+    input clk,
     input [31:0] luindex,
     input [31:0] stor_index,
     input [31:0] stor_data_in,
     input w_enable,
-    output [31:0] stor_data_out
+    output [31:0] stor_data_out,
+    output [31:0] data_replacement_idx,
+    output [512:0] data_replacement,
+    output data_replacement,
+    output data_rep_w_enable
 );
     //512 bit data storage (64 bytes)
-    reg [511:0] data [15:0];
+    reg [511:0] data [3:0];
     //each has a 22 bit tag that needs to be accessed
-    reg [21:0] tag [15:0];
+    reg [21:0] tag [3:0];
     //now I need to map a valid bit in:
-    reg [15:0] valid [15:0];
+    reg valid [3:0];
     wire [21:0] tag_extr = luindex[31:10];
     wire [3:0] idx = luindex[9:6];
     wire [5:0] offset = luindex[5:0];
 
     //implementation of least recently used policy
-    reg [3:0] LRU [15:0];
-    always @(*) begin
+    always @(posedge clk) begin
         if (w_enable) begin
-            if(tag[idx] == tag_extr) begin
-                data[idx] = tag;
-                for(i = 0; i < 16; i = i +1) begin
-                    LRU [i] = LRU [i] + 1;
+            if(valid[idx]) begin
+                if(tag[idx] == tag_extr) begin
+                    data[idx] = tag;
+                end else begin
+                    L2_cache()
                 end
-                LRU [idx] = 0;
-            end else begin
-                det_most_used 
-                L2_cache()
             end
         end
     end
 endmodule
 
+
 module L2_cache(
     input clk,
-    input [31:0] w_data,
-    input [31:0] index,
     input w_enable,
-    input mem_hi_up,
-    output [511:0] mem_hi_up_result
+    input replacement_needed,
+    input [512:0] data_in,
+    input [31:0] idx,
+    output reg [512:0] data_out,
+    output data_replacement,
+    output [31:0] data_replacement_idx,
+    output data_replacement_w_enable
 );
-    reg []
+    reg [511:0] data [6:0];  //larger cache than l1, 8x more data here
+    reg [18:0] index [6:0];
+    wire [6:0] idx_l2 = idx [12:6];
+    wire [18:0] tag_l2 = idx[31:13];
+    always @(posedge clk) begin
+        if(replacement_needed) begin
+            if(index[idx_l2] == tag_l2) begin
+                data_out <= data[idx_l2];
+                if(w_enable)
+            end
+            else begin
+                data_replacement_idx <= idx;
+                if(w_enable) begin
+                    data_replacement_w_enable <= 1;
+                end
+            end
+        end
+    end 
 endmodule
 
 
