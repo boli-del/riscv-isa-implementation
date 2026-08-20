cc39b2708bc64ff03ab74a7230e6810c42e89712
bob <bobli8203@gmail.com>
2026-08-20 12:01:41 -0400

    added tb and i type instr support

--- rtl/src/instruction_decode.v
+++ rtl/src/instruction_decode.v
@@ -12,7 +12,7 @@ module instruction_dec(
     output [2:0] funct3,
     output [6:0] opcode,
     output [3:0] mode,
-    output [2:0] immsel,
+    output [31:0] imm,
     output bsel
 );
     assign funct7 = instr_code [31:25];
@@ -31,16 +31,22 @@ module ID_pipeline_reg(
     input [3:0] mode,
     input [31:0] rs1val,
     input [31:0] rs2val,
+    input [31:0] immval,
+    input bsel,
     output reg [4:0] storage_rd,
     output reg [3:0] storage_mode,
     output reg [31:0] storage_rs1val,
-    output reg [31:0] storage_rs2val
+    output reg [31:0] storage_rs2val,
+    output reg [31:0] storage_immval,
+    output reg storage_bsel
 );
     always @(posedge clk) begin
         storage_rd <= rd;
         storage_mode <= mode;
         storage_rs1val <= rs1val;
         storage_rs2val <= rs2val;
+        storage_immval <= immval;
+        storage_bsel <= bsel;
     end
 endmodule
 
@@ -49,27 +55,73 @@ module det_r_alu_op(
     input [2:0] funct3,
     input [6:0] funct7,
     input [6:0] opcode,
-    output reg [3:0] alu_op_code
-    output reg [2:0] immsel);
+    input [24:0] instr_need,
+    output reg [3:0] alu_op_code,
+    output reg [31:0] imm,
+    output reg b_sel);
 
     task det_immsel;
         input [6:0] op_code;
         output [2:0] immsel_res;
+        output bsel;
 
         begin
             case(opcode)
-                7'b0010011: immsel_res = 3'b000;
-                7'b0000011: immsel_res = 3'b001;
-                7'b0100011: immsel_res = 3'b010;
-                7'b1100011: immsel_res = 3'b011;
-                7'b1101111: immsel_res = 3'b100;
+                7'b0010011: begin
+                    immsel_res = 3'b000;
+                    b_sel = 1;
+                end
+                7'b0000011: begin
+                    immsel_res = 3'b001;
+                    b_sel = 1;
+                end
+                7'b0100011: begin
+                    immsel_res = 3'b010;
+                    b_sel = 1;
+                end
+                7'b1100011: begin
+                    immsel_res = 3'b011;
+                    b_sel = 1;
+                end
+                7'b1101111: begin
+                    immsel_res = 3'b100;
+                    b_sel = 1;
+                end
+                else: begin
+                    b_sel = 0;
+                    imm_res = 3'b111;
+                end
             endcase
         end
     endtask
 
+    task immgen;
+        input [24:0] instr_need;
+        input [2:0] immsel;
+        output [31:0] imm;
+        case(immsel)
+            3'b000: begin
+                imm = instr_need[24:14];
+            end
+            3'b001: begin
+                imm = {instr_need[24:19], instr_need[4:0]}; 
+            end
+            3'b010: begin
+                imm = {instr_need[12], instr_need[0], instr_need[23:19], instr_need[4:1]};
+            end
+            3'b011: begin
+                imm = instr_need[24:5];
+            end
+            3'b100: begin
+                imm = {instr_need[24], instr_need[12:5], instr_need[13], instr_need[23:14]};
+            end
+        endcase
+    endtask
+
     always @(*)begin
-        det_immsel selct_imm(opcode, immsel);
-        if(funct3 == 0)begin
+        det_immsel selct_imm(opcode, immsel, b_sel);
+        immgen immgen(op);
+        if (funct3 == 0) begin
             if(funct7 == 0) begin
                 alu_op_code = 0;
             end
