58010116a84baf8db081634bfe726b1e6826b982
Bob Li <bobli8203@gmail.com>
2026-08-23 22:07:20 -0400

    behavorial add test

--- testbenches/test_imm.py
+++ testbenches/test_imm.py
@@ -9,3 +9,25 @@ async def test_i_positive(dut){
     await RisingEdge(dut.clk)
     dut.await Timer(1.2, unit = 'ns')
 }
+
+@cocotb.test()
+async def test_i_add(dut):
+    clk = Clock(dut.clk, 2, unit = 'ns')
+    clk.start()
+    await Timer(1.2, unit = 'ns')
+    op_code = 0x00A30293
+    dut.rst_n.value = 1
+    dut.pc.value = 0
+    dut.instr_mem[0].value = op_code
+    await RisingEdge(dut.clk)
+    await Timer(0.2, unit = 'ns')
+    assert(dut.inst_code.value == op_code)
+    await RisingEdge(dut.clk)
+    await Timer(0.2, unit = 'ns')
+    assert(dut.rd.value == 5)
+    assert(dut.rs1.value == 6)
+    assert(dut.imm == 10)
+    assert(dut.bsel == 0)
+    await RisingEdge(dut.clk)
+    await Timer(0.2, unit = 'ns')
+    assert(dut.reg_file[5].value = 10)
