ad5c7c2353438aa2f9883c3d2ce10153fa31a6a5
boli-del <bobli8203@gmail.com>
2026-09-10 12:34:59 -0400

    added sv conversion to scala, added scala based synthetic trace generation, started dut based synthetic trace test on synthetic based gens

---
+++ chisel/src/main/scala/EmitVerilog.scala
@@ -0,0 +1,8 @@
+// src/main/scala/EmitVerilog.scala
+import circt.stage.ChiselStage
+import java.io.PrintWriter
+
+object EmitVerilog extends App {
+  val verilog = ChiselStage.emitSystemVerilog(new Cache(numWays = 4, numSets = 8, blockSize = 512))
+  new PrintWriter("cache_4way_8set_512blk.sv") { write(verilog); close() }
+}
