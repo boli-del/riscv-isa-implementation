256c814ae7b9daaa8fe4bf54bfc82c2a7267ba52
boli-del <bobli8203@gmail.com>
2026-09-10 09:16:57 -0400

    fixed non-compilation test error and verified expected behavior works as intended

--- chisel/src/test/scala/cache_test.scala
+++ chisel/src/test/scala/cache_test.scala
@@ -6,7 +6,7 @@ class CacheSpec extends AnyFlatSpec{
     import reg_refills.State
     import reg_refills.State._
     it should "fetch_stall correctly" in {
-        simulate(new Cache(3, 3, 512)){ dut =>
+        simulate(new Cache(8, 8, 512)){ dut =>
             dut.io.w_enable.poke(0.U)
             dut.io.data_in.poke(0.U)
             dut.io.location.poke(32.U)
@@ -22,7 +22,7 @@ class CacheSpec extends AnyFlatSpec{
         }
     }
     it should "replace correctly" in {
-        simulate(new Cache(3, 3, 512)){ dut =>
+        simulate(new Cache(8, 8, 512)){ dut =>
             dut.io.w_enable.poke(0)
             dut.io.data_in.poke(0)
             dut.io.location.poke(56.U)
@@ -32,11 +32,11 @@ class CacheSpec extends AnyFlatSpec{
             dut.clock.step(1)
             assert(dut.io.state.peekValue().asBigInt == sNot_Hit_Valid.litValue);
             dut.io.stalled_inputloc.poke(dut.io.stalled_location.peekValue().asBigInt)
-            dut.io.stalled_wenable.poke(dut.io.stalled_input_wenable.peekValue().asBigInt)
+            dut.io.stalled_input_wenable.poke(dut.io.stalled_wenable.peekValue().asBigInt)
             dut.clock.step(1)
             dut.io.replacement_incoming.poke(1.U)
             dut.io.stalled_inputloc.poke(dut.io.stalled_location.peekValue().asBigInt)
-            dut.io.stalled_wenable.poke(dut.io.stalled_input_wenable.peekValue().asBigInt)
+            dut.io.stalled_input_wenable.poke(dut.io.stalled_wenable.peekValue().asBigInt)
             dut.io.data_in.poke(1234.U)
             dut.io.location.poke(56.U)
             dut.clock.step(1)
@@ -49,6 +49,14 @@ class CacheSpec extends AnyFlatSpec{
             dut.io.w_enable.poke(dut.io.stalled_input_wenable.peekValue().asBigInt)
             dut.clock.step(1)
             assert(dut.io.state.peekValue().asBigInt == sSearch.litValue);
+            dut.io.location.poke(dut.io.stalled_inputloc.peekValue().asBigInt)
+            dut.io.w_enable.poke(dut.io.stalled_input_wenable.peekValue().asBigInt)
+            dut.clock.step(1)
+            assert(dut.io.state.peekValue().asBigInt == sHit_and_return.litValue);
+            dut.io.location.poke(dut.io.stalled_inputloc.peekValue().asBigInt)
+            dut.io.w_enable.poke(dut.io.stalled_input_wenable.peekValue().asBigInt)
+            dut.clock.step(1)
+            dut.io.data_out.expect(0.U)
         }
     }
 }
