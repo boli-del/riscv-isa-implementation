31f74edeec24a6904204173bafe07f4f838ae6f0
boli-del <bobli8203@gmail.com>
2026-09-11 08:59:07 -0400

    first form of evaluator

--- chisel/src/main/scala/EmitVerilog.scala
+++ chisel/src/main/scala/EmitVerilog.scala
@@ -4,5 +4,5 @@ import java.io.PrintWriter
 
 object EmitVerilog extends App {
   val verilog = ChiselStage.emitSystemVerilog(new Cache(numWays = 4, numSets = 8, blockSize = 512))
-  new PrintWriter("cache_4way_8set_512blk.sv") { write(verilog); close() }
+  new PrintWriter("cache_4way_8set_512blk.sv") { write(verilog); close()}
 }
